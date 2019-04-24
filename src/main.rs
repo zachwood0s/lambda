7c9a91fdc16efc95e2ba13ffa67bedd5df48ae29
zachwood0s <zach@hayzak.com>
2019-04-24 14:08:45 -0500

    added command line argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+extern crate clap;
+use clap::{Arg, App};
 
 fn main(){
-    println!("Hello World");
+    let matches = App::new("lambda")
+                    .version("0.0.1")
+                    .author("Zach W. <zach@hayzak.com>")
+                    .about("Lambda Calculus Implementation")
+                    .arg(Arg::with_name("MODE")
+                            .help("What mode to run the program in")
+                            .index(1)
+                            .possible_values(&["repl", "make"])
+                            .required(true))
+                    .get_matches();
+
+    match matches.value_of("MODE").unwrap(){
+        "repl" => println!("chose repl mode"),
+        "make" => println!("chose make mode"),
+        _ => unreachable!()
+    }
 }
