5fefda0c59a13edf88d2c2086ef637aa6ae0b2dd
zachwood0s <zach@hayzak.com>
2019-04-29 14:01:16 -0500

    more repl changes

--- src/repl.rs
+++ src/repl.rs
@@ -6,7 +6,6 @@ use parser::Parser;
 use parser::Visitor;
 use parser::ParseNode;
 use parser::GrammarItem;
-
 use colored::*;
 
 static INDENT_AMOUNT : i32 = 2;
@@ -77,7 +76,7 @@ impl Visitor<()> for PrintVisitor{
       println!("{} {}", "Assignment:".green().underline(), name.cyan());
       self.print_indent();
       println!("{}", "-Value:".bright_green().italic());
-      self.current_indent += 1;
+      self.current_indent += 1; 
       self.visit(expr);
       self.current_indent -= 1;
     }
@@ -96,37 +95,125 @@ impl Visitor<()> for PrintVisitor{
   }
 }
 
+enum PromptResult {
+  Command(String, Option<String>),
+  Input(String), 
+  InvalidCommand
+}
+
+struct Prompt<'a>{
+  options: Vec<PromptOption<'a>>
+}
+
+impl<'a> Prompt<'a>{
+  fn new() -> Self {
+    Prompt{options: vec!()}
+  }
+
+  fn option(mut self, option: PromptOption<'a>) -> Self {
+    self.options.push(option);
+    self
+  }
+
+  fn show(self) -> PromptResult {
+    let mut input = String::new();
+    print!("> ");
+    io::stdout().flush().unwrap();
+
+    io::stdin().read_line(&mut input)
+      .ok()
+      .expect("Couldn't read line");
+
+    let input = input.trim().to_string();
+
+    if input.starts_with(":") {
+      self.handle_command(input)
+    } else {
+      PromptResult::Input(input)
+    }
+  }
+
+  fn handle_command(self, input: String) -> PromptResult {
+    let command_parts = &input[1..].split(" ").collect::<Vec<_>>();
+    let command_string = command_parts[0].to_string();
+
+    if command_string == "h" || command_string == "help" {
+
+      PromptResult::Command("HELP".to_string(), None)
+    }
+    else{
+      let option = self.options.iter().find(
+        |x| x.name == command_string || x.short_name == Some(&command_string));
+      
+      match option {
+        Some(command) => PromptResult::Command(command.name.to_string(), Some(command_parts[1..].join(" "))),
+        None => PromptResult::InvalidCommand
+      }
+    }
+
+  }
+}
+
+struct PromptOption<'a> {
+  name: &'a str,
+  help: Option<&'a str>,
+  short_name: Option<&'a str>,
+}
+
+impl<'a> PromptOption<'a> {
+  fn with_name(name: &str) -> PromptOption{
+    PromptOption { name: name, help: None, short_name: None}
+  }
+
+  fn help(mut self, help: &'a str) -> Self{
+    self.help = Some(help);
+    self
+  }
+
+  fn short(mut self, short_name: &'a str) -> Self{
+    self.short_name = Some(short_name);
+    self
+  }
+}
+
 pub fn start(){
   main_loop();
 }
 
 fn main_loop(){
 
-  let mut input = String::new();
+  let prompt = Prompt::new()
+                .option(PromptOption::with_name("type")
+                  .short("t")
+                  .help("Displays the type of the expression provided "));
 
-  print!("> ");
-  io::stdout().flush().unwrap();
-  input.clear();
+  let input = prompt.show();
 
+  match input {
+    PromptResult::Input(expr) => {
+      let lexer = Lexer::new(expr.as_str());
+      let mut parser = Parser::new(lexer);
+      let mut printer = PrintVisitor::new();
 
-  io::stdin().read_line(&mut input)
-    .ok()
-    .expect("Couldn't read line");
+      let result = parser.parse()
+          .or_else(|_| {
+            parser.reset_lexer();
+            parser.parse_expr()
+          });
 
-  println!("{}", input);
-  let lexer = Lexer::new(input.as_str());
-  let mut parser = Parser::new(lexer);
-  let mut printer = PrintVisitor::new();
+      match result {
+        Ok(ast) => printer.visit(&ast),
+        Err(e) => println!("Error parsing: {:?}",e)
+      }
+    }
 
-  let result = parser.parse()
-      .or_else(|_| {
-        parser.reset_lexer();
-        parser.parse_expr()
-      });
-
-  match result {
-    Ok(ast) => printer.visit(&ast),
-    Err(e) => println!("Error parsing: {:?}",e)
+    PromptResult::Command(c, rest) =>  
+      match &*c {
+        "h" => println!("help"),
+        _ => println!("Other")
+      }
+    
+    PromptResult::InvalidCommand => println!("invalid")
   }
 }
 
